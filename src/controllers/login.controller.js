1cffcafedcb854b07193b285efecb25432bc9fa1
Raghav Mandowara <raghav.mandowara@accolitedigital.com>
2021-07-11 11:05:40 +0530

    roles added

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,18 +1,28 @@
 var admin = require('firebase-admin');
 var firebase = require('firebase');
 function signup (req, res)  {
+    console.log(req.body)
     let email = req.body.email;
     let password = req.body.password;
+    let role = req.body.role;
     admin.auth().createUser({
         email,
         emailVerified: false,
         password,
     }).then((val)=>{
-        res.status(200);
-        res.send({
+        admin.firestore().collection('users').doc(val.uid).set({
+            role,
+            'accepted':false
+        }).then((val)=>{
+            res.status(200);
+             res.send({
             "code":200,
             "message":"User created successfully"
+        },(error)=>{
+            console.log(error);
+            res.send(error);
         });
+        })
         
     },(error)=>{
         res.send(error);
@@ -22,12 +32,20 @@ function signup (req, res)  {
 function login (req, res)  {
     let email = req.body.email;
     let password = req.body.password;
+    
 
     firebase.auth().signInWithEmailAndPassword(email,password).then((userCredentials)=>{
         let user =userCredentials.user;
-        res.send({
-            user
+        admin.firestore().collection('users').doc(user.uid).get().then((details)=>{
+            console.log(details);
+            res.send({
+                user,
+                details
+            })
+        },(error)=>{
+            res.send(error);
         })
+        
     },(error)=>{res.send(error); }
     )
 }
